531a2f53291d2350f54d9a1dfdd1992fd7a6cc9d
AtherCoder99 <a.ngaming102734@gmail.com>
2026-05-29 23:30:41 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import'./index.css'
+
+// export const  App = () => {
+//    return(
+//     <h1>Hello Aslam walikum</h1>
+//    )
+// };
+
+
+
+const App = () => {
+  
+  const element = <h1>Aslam walikum</h1>
+  
+  return (
+    <>
+    
+    <div> 
+      {element}
+    </div>
+     </>
+  );
+};
+
+export default App;
